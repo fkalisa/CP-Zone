7215d61ce6ebfd0d8b97b8844f4a2f8c24a95b10
fkalisa <30494790+fkalisa@users.noreply.github.com>
2020-09-05 13:31:40 +0200

    Update ds-priortyQueue.cpp

--- ds-priortyQueue.cpp
+++ ds-priortyQueue.cpp
@@ -3,8 +3,10 @@ using namespace std;
 
 
 int kThGreaterElement(int arr[], int n, int k) {
+	/*
+	known as maximum priority queue
+	*/
 	priority_queue<int, std::vector<int>, greater<int>> q;
-
 
 	for (int i = 0; i < n; ++i)
 	{
@@ -23,7 +25,5 @@ int main() {
 	int arr[] = {2, 1, 5, 3, 6, 4, 8, 9, 7, 10};
 	int n = 10;
 
-
 	cout << "answer : " << kThGreaterElement(arr, n, 4);
-
 }
