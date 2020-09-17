6db63108fa73a10e83c94d823e9b4f4a99712806
Flora K <akaliza@gmail.com>
2020-09-17 22:32:24 +0200

    LCS

---
+++ graph-and-tree/algorithm-LowestCommonAncestor.cpp
@@ -0,0 +1,112 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+extern const int N = 10000;
+vector<int> adjs[N + 1];
+int lev[N + 1];
+int parent[N + 1];
+
+void init(int n) {
+	for (int i = 0; i < n - 1; ++i)
+	{
+		int u, v;
+		cin >> u >> v;
+		adjs[u].push_back(v);
+		adjs[v].push_back(u);
+
+	}
+}
+
+void dfs(int node, int par, int level) {
+
+
+	lev[node] = level;
+	parent[node] = par;
+
+	for (int i = 0; i < adjs[node].size(); ++i)
+	{
+		if (adjs[node][i] != par)
+		{
+			dfs(adjs[node][i], node, level + 1);
+		}
+	}
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+
+//for getting input from input.txt
+	freopen("input.txt", "r", stdin);
+
+//for writting output to output.txt
+	freopen("output.txt", "w", stdout);
+#endif
+
+	/*
+
+	    searching LCA of 12 and 11
+
+	    solution : 3
+
+	    inputs :
+
+	    13
+		1 2
+		1 3
+		1 4
+		3 5
+		3 6
+		5 7
+		5 8
+		6 9
+		6 10
+		6 11
+		8 12
+		8 13
+	*/
+
+	int n;
+	cin >> n;
+
+	init(n);
+	dfs(1, 0, 0);
+
+
+	int i = 12;
+
+	std::vector<int> v1;
+	while (i != 1) {
+		i = parent[i];
+		v1.push_back(i);
+	}
+
+
+	std::vector<int> v2;
+	int j = 11;
+	while (j != 1) {
+		j = parent[j];
+		v2.push_back(j);
+	}
+
+	sort( v1.begin(), v1.end());
+
+	sort(v2.begin(), v2.end());
+
+	int result = -1;
+	for (int i = 0; i < n; ++i)
+	{
+		if (v2[i] == v1[i])
+		{
+			result = v1[i];
+		} else {
+			break;
+		}
+	}
+
+	cout << "answer : " << result << endl;
+
+
+	return 0;
+
+}
